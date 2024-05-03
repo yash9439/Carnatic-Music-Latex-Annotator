7658db75df38af5e503e6d6af9aec77adbf78a94
yash bhaskar <yash9439@gmail.com>
2024-05-03 23:10:21 +0530

    Cross Added

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const outputElement = document.querySelector('#output');
 const textInput = document.querySelector('#textInput');
 let latexOutput = '';
 
-const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Subscript', 'SuperScript', 'Left SuperScript' , 'Single Overline', 'Double Overline', 'Triple Overline', 'Dot', 'Double Dot', 'UnderDot', 'Double UnderDot', 'Single Underline', 'Curve', '⌣', '⌢', '| above', ',', '.', ';', '-', '=', '(', ')', '\'', '"', '/', '+', '*'];
+const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Subscript', 'SuperScript', 'Left SuperScript' , 'Single Overline', 'Double Overline', 'Triple Overline', 'Dot', 'Double Dot', 'UnderDot', 'Double UnderDot', 'Single Underline', 'Curve', '⌣', '⌢', '| above', '×', ',', '.', ';', '-', '=', '(', ')', '\'', '"', '/', '+', '*'];
 
 const swara1Keys =  ['S', 'R1', 'R2', 'G1', 'G2', 'M1', 'M2', 'P', 'D1', 'D2', 'N1', 'N2'];
 
